cff8976d72b287ba6445e117cbd613e1733f93a9
numandotdev <numandotdev@gmail.com>
2023-12-30 15:25:00 +0530

    Dollar to Inr converter Console App

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Welcome to Currency Converter Console App");
+    println!("Enter your amount $");
+    
+    io::stdin().read_line(&mut input).expect("Wrong Input");
+
+    let dollar: f32 = input
+        .trim()
+        .parse()
+        .expect("Failed to parse string into float");
+
+    let inr: f32 = dollar * 83.44;
+
+    println!("${}  is ₹{}", dollar, inr);
 }
