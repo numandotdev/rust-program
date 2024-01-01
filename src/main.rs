23b9d4e43c781fd3f30c467459e2743a9b53246f
numandotdev <numandotdev@gmail.com>
2024-01-01 16:17:41 +0530

    created object

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
-use std::{io, any::{type_name, Any}, result};
+use std::{ /*any::{type_name, Any}, io,*/ vec };
 
-mod currency;
+// mod currency;
 
-use currency::currency_tool;
+// use currency::currency_tool;
 // #[path = "./maths/maths_program.rs"]
 // mod maths;
 
 // use maths::is_even;
 
 fn main() {
-    let result = currency_tool();
-    println!("{}", result);
+    // let result = currency_tool();
+    // println!("{}", result);
+
+    let user_data = ["Numan", "Kawsar", "Rezwan", "John", "Elisa"];
+
+    // println!("{:?}", users)
+    // for user in users {
+    //     println!("{}", user)
+    // }
+
+    let user = User::new_user("Tony", 23);
+    user.display_user();
 }
+
+struct User {
+    name: String,
+    age: u32,
+}
+
+impl User {
+    fn new_user(name:&str, age: u32) -> User {
+        User {
+            name: String::from(name),
+            age: age,
+        }
+    }
+    fn display_user(&self) {
+        println!("Name: {}, Age: {}", self.name, self.age);
+    }    
+}
+
